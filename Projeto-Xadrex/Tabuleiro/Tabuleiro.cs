ef6d4df041b73380e1265071e5752d9c4c13133d
Felipe Mendes <felipe.m.alex@hotmail.com>
2019-07-16 14:12:20 -0300

    Fase 06: Excepitions Tabuleiro

--- Projeto-Xadrex/Tabuleiro/Tabuleiro.cs
+++ Projeto-Xadrex/Tabuleiro/Tabuleiro.cs
@@ -18,10 +18,37 @@
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)//recebe uma posiçao pos
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);// caso a posicao nao valide o metodo corta e lanca uma exception
+            return peca(pos) != null;// retorna as posicoes diferentes de nullas caso valide
+        }
         public void colocarPeca(Peca p, Posicao pos)// coloca a peça no tabuleiro
         {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao=pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linha<0|| pos.linha>=linhas|| pos.coluna<0|| pos.coluna>=colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))// se não for valida  
+            {
+                throw new TabuleiroException("Posição invalida!");
+            }
         }
     }
 }
